18deb64dcf576b288c0ef49b03d09e4d3139a30c
José Roldán Jiménez <joserj88@gmail.com>
2021-02-14 15:40:00 +0100

    Create alias.sh
    append alias

---
+++ alias.sh
@@ -0,0 +1,10 @@
+#!/bin/sh  
+echo "alias lt='ls -lGt'
+alias ll='ls -lG'
+alias la='ls -la'
+alias cls='clear'
+alias ip='curl ipecho.net/plain; echo'
+alias h="history"
+alias c="clear"
+alias o="open"
+alias n="nano"" >> $HOME/.zshrc
