045dece90ba82369a511874726c057115e64b856
José Roldán Jiménez <joserj88@gmail.com>
2021-02-14 17:04:26 +0100

    Update docker.sh
    integrate docke composer

--- docker.sh
+++ docker.sh
@@ -1,6 +1,10 @@
 #/bin/sh
 
 echo "Install Dockers:"
-#sh -c "$(curl -fsSL https://get.docker.com -o get-docker.sh)"
-#sh -c "$(sudo sh get-docker.sh)"
+sh -c "$(curl -fsSL https://get.docker.com -o get-docker.sh)"
+sh -c "$(sudo sh get-docker.sh)"
 rm ./get-docker.sh
+
+echo  "Install Docker composer:" 
+sh -c "$(sudo apt-get -y install libffi-dev libssl-dev python3-dev python3 python3-pip)"
+sh -c "$(sudo pip3 -v install docker-compose)"
